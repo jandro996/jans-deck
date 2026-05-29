e642e8ff8007f1410003c1f5ad8488d398436c00
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2026-05-29 11:13:39 +0200

    Add jans-menu.sh launcher using Homebrew Python framework for macOS GUI

---
+++ jans-menu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Launch jans menu bar app using the Homebrew Python framework (required for macOS GUI)
+exec ~/research/jans/.venv-menu/bin/python3 ~/research/jans/jans/menubar.py "$@"
